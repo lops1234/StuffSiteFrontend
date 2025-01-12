6276656582f7abc9b0e900bb2dd3f9cc0e8442e6
Kaspars Avotins <kaspars.avotins@gmail.com>
2025-01-12 13:08:43 +0200

    store + reducer for user

---
+++ src/store/user.reducer.ts
@@ -0,0 +1,26 @@
+﻿import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import IUser from '../types/user.type';
+
+interface UserState {
+    user: IUser | null;
+}
+
+const initialState: UserState = {
+    user: null,
+};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser(state, action: PayloadAction<IUser>) {
+            state.user = action.payload;
+        },
+        clearUser(state) {
+            state.user = null;
+        },
+    },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+export default userSlice.reducer;
