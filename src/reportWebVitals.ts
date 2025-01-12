f7ac4075d9adca16d7c7b88a21ae1f5fab2c3fca
Kaspars Avotins <kaspars.avotins@gmail.com>
2025-01-12 13:09:36 +0200

    report vitals stuff

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,15 @@
+﻿import { MetricType } from "web-vitals";
+
+const reportWebVitals = (onPerfEntry?: (metric: MetricType) => void) => {
+    if (onPerfEntry) {
+        import("web-vitals").then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
+            onCLS(onPerfEntry);
+            onINP(onPerfEntry);
+            onFCP(onPerfEntry);
+            onLCP(onPerfEntry);
+            onTTFB(onPerfEntry);
+        });
+    }
+};
+
+export default reportWebVitals;
